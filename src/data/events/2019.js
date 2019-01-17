d9dccfc99cd5cbfacf9a9f009dcb21f51d367d60
Nic Ollis <nic@oll.is>
2019-01-17 15:31:05 -0500

    added new blog post

--- src/data/events/2019.js
+++ src/data/events/2019.js
@@ -2,6 +2,14 @@ import { MLProject } from '../../models/projects'
 
 export default [
   new MLProject({
+    title: 'macOS Machine Learning in 2019',
+    detail: 'As we start 2019, let\'s review what machine learning on macOS looks like now and how it might improve.',
+    date: 'Januaray 17 2019',
+    link: 'https://www.bignerdranch.com/blog/macos-machine-learning-in-2019/',
+    tech: ['PlaidML', 'Keras'],
+    eventType: 'blog'
+  }),
+  new MLProject({
     title: 'Digit Recognizer',
     detail: 'Solutions to Kaggle\'s MNSIT computer vision problem scoring in the top 12%',
     date: 'Januaray 10 2019',
