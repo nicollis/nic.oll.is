6c78b198e73a27bc952b47ba36805f8c18d043b6
Nic Ollis <nic@oll.is>
2020-04-21 06:16:17 -0400

    Fixing content length

--- src/data/events/2020.js
+++ src/data/events/2020.js
@@ -5,7 +5,7 @@ export default [
        title: 'Validating Proof of Concepts before Data Collection',
        detail: 'How we can validate an idea before collecting real world data',
        date: 'April 14, 2020',
-       link: 'https://www.bignerdranch.com/blog/implementing-swish-activation-function-in-keras/',
+       link: 'https://www.bignerdranch.com/blog/validating-proof-of-concepts-before-data-collection/',
         tech: ['Turi Create', 'Computer Vision'],
        eventType: 'blog'
     }),
@@ -13,7 +13,7 @@ export default [
        title: 'The Scope of Machine Learning',
        detail: 'Lets try to pin a definition of the what projects fall under machine learning',
        date: 'February 25, 2020',
-       link: 'https://www.bignerdranch.com/blog/implementing-swish-activation-function-in-keras/',
+       link: 'https://www.bignerdranch.com/blog/the-scope-of-machine-learning/',
        eventType: 'blog'
     }),
     new MLProject({
